d5c9e9242ef6511164ddb58171c9c438d6c02028
Wolfgang Jose Santamaria Cordoba <santamaria.wolfgang@gmail.com>
2020-12-12 22:22:26 -0500

    Creacion backend

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-/*en caso de  hacer uso con el directorio controlador se 
-debe importar como se observa en la siguiente linea, con el nombre del archivo js
-que contiene la logica */
-//const controller = require('./controller/nombredelcontrollador.js');
+const controller = require('./controller/controller.js');
 const express = require('express');
 const db = require('./models');
 const app = express()
@@ -20,6 +17,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 /*se debe contar un una ruta por medio de método post para el inicio de sesión de la siguiente manera:
 '/api/auth/signin'
 */
+
+app.post('/api/users', (req,res) => {
+    db.user.findAll().then(users => res.json(users))
+});
+
+app.post('/api/auth/signin', controller.signin);
+
 app.get('/', function(req, res) {
     console.log("Estructura base del proyecto backend");
     res.send("Estructura base del proyecto backend");
